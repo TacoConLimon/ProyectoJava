9125caf4a7327e59de2f9c023ce2f448cd068f35
TacoConLimon <juandavidgove24@gmail.com>
2023-11-20 20:21:23 -0500

    Subida proyecto Spring

---
+++ src/main/java/com/example/trabajoJava/TrabajoJava/TrabajoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabajoJava.TrabajoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trabajoJava/TrabajoJava/TrabajoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabajoJava.TrabajoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
